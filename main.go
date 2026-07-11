efb53093da538ba5dcdd4c2d1f9de134efba9c43
bluizads <268444739+bluizads@users.noreply.github.com>
2026-07-11 12:10:21 -0300

    Inserindo mais produtos na main

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ func main() {
 
 	servico := NovoServicoPedido(repoProduto, repoPedido)
 
-	repoProduto.Salvar(Produto{ID: "P001", Nome: "Notebook", Preco: 3500, Estoque: 5})
+	repoProduto.Salvar(Produto{ID: "P001", Nome: "Notebook", Preco: 3500.00, Estoque: 5})
+	repoProduto.Salvar(Produto{ID: "P002", Nome: "Mouse", Preco: 80.00, Estoque: 10})
+	repoProduto.Salvar(Produto{ID: "P003", Nome: "Teclado", Preco: 180.00, Estoque: 8})
 
 	itens := []ItemPedido{
 		{ProdutoId: "P001", Quantidade: 1},
